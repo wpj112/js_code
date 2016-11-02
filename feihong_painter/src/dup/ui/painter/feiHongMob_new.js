8f4ef53e6881ec740e4485f3b8239930a68b352b
wpj112 <wpj112@gitlub.com>
2016-11-02 15:50:40 +0800

    更新一版嵌入式暂时可用版本

--- feihong_painter/src/dup/ui/painter/feiHongMob_new.js
+++ feihong_painter/src/dup/ui/painter/feiHongMob_new.js
@@ -35,7 +35,6 @@ oojs.define({
     windowLoadedFlag:0,
     preClientY:0,
     adsToBottom:0,
-    bdHeight:0,
 
     /**
      * Painter入口
@@ -54,8 +53,6 @@ oojs.define({
         var showReq = '&feihong_type=1'; 
         this.sendMonitor(showReq);
         this.adSlotInfo = slotInfo;
-        this.bdHeight = this.bdom.getScrollHeight();
-
         slotInfo.paramObj.coa += '%26tab%3D' + '0' + '%26ptn%3D' + '0';
         // 用于显示加载中图片
         this.addCss(' @-webkit-keyframes bd-loadRotate {from {-webkit-transform:rotate(0deg);}'
@@ -69,26 +66,42 @@ oojs.define({
             + 'animation-name: bd-loadRotate; animation-duration: 1.3s; animation-iteration-count: infinite; '
             + 'animation-timing-function: linear; }');
 
-        var main_item = this.createBackupWrapper(slotInfo.slotId);
-        this.addBackBtn(10, 100, slotInfo);
-        //var fhTopitem = document.createElement('div');
-        // 做一个分界点备用
-        //fhTopitem.id = 'fh_top_item';
-        //document.body.appendChild(fhTopitem);
+        var fhTopitem = document.createElement('div');
 
-        //e.renderBanner(slotInfo);
+        // 做一个分界点备用
+        fhTopitem.id = 'fh_top_item';
+        document.body.appendChild(fhTopitem);
+
+        e.renderBanner(slotInfo);
         this.sendAdsRequest();
         this.sendAdsRequest();
         //window.onscroll = this.on_scroll_fun();
         // 数据处理中心,封装所有数据，后续所有数据从templateData获取
 
-        this.bdom.bind(window, "scroll", function () {
+/***
+        this.bdom.bind(window, 'scroll', function (){
+            e.changeBanner(e.adSlotInfo, e);
+        });
+***/
 
+        this.bdom.bind(window, "scroll", function () {
             var slotInfo = e.adSlotInfo;
-            var topItem = document.getElementById('fh_top_item_' + slotInfo.slotId);
+            var topItem = document.getElementById('fh_top_item');
+            // 文档高度 -(滚到条位置 + 流量器窗口高度) >30
+            var topItem = document.getElementById('fh_top_item');
             var Stop = e.bdom.getScrollTop();
             var Cheight = e.bdom.getClientHeight();
-
+            var Sheight = e.bdom.getScrollHeight();  
+            var adsHeight = topItem.clientHeight;
+            var adsToBottom = 0;
+            if (window.fhShowStatus === 1){
+                adsToBottom = Sheight - adsHeight;
+            }
+            if (((Sheight - (Stop + Cheight)) < (Cheight + adsToBottom)) && e.allowSend) {
+                e.sendAdsRequest();
+                if (e.tabIdx % 4 === 0)  e.sendAdsRequest();
+            }
+            
             var topHeight = topItem.offsetTop;
             
             //for send see the feihong
@@ -96,7 +109,7 @@ oojs.define({
                 e.sendMonitor('&feihong_type=3');
                 e.sendMonitorFlag = 0;
             }
-/****
+
             //if ((e.fhTopflag === 0) && (document.readyState == 'complete') &&(topHeight <= (Stop + Cheight - 50))) {
             //for auto go to top
             if ((e.fhTopflag === 0) && (document.readyState == 'complete') &&(topHeight <= (Stop + Cheight - 50))) {
@@ -123,7 +136,6 @@ oojs.define({
                 window.fhShowStatus = 1;
                 e.renderFloatBanner(e);
             }
-            ****/
         });
     },
     sendAdsRequest: function () {
@@ -266,6 +278,45 @@ oojs.define({
         topItem.appendChild(bannerItem);
         //document.body.appendChild(bannerItem);
     },
+    renderFloatBanner:function (e){
+        var slotInfo = e.adSlotInfo;
+        var bannerImg = document.getElementById('Banner_img_item');
+        var screenWidth = slotInfo.paramObj.rw;        
+        var bannerItem = document.createElement('div');
+
+        bannerItem.id = 'fh_banner_item_fixed';
+        bannerItem.style.display = 'block';
+        bannerItem.style.position = 'fixed';
+        bannerItem.style.height = screenWidth * 94 / 640 + 'px';
+        bannerItem.style.width = screenWidth + 'px';
+        bannerItem.style.top = '0px';
+        bannerItem.style.left = '0px';
+        bannerItem.style.zIndex = 2147483647;
+        bannerItem.style.height = screenWidth *94 / 640 + 'px';
+
+        var imgBanner = document.createElement('img');
+        imgBanner.id = 'Banner_img_item_fixed';
+        imgBanner.src = 'http://t11.baidu.com/it/u=3620413495,2142661669&fm=76';
+        imgBanner.style.display = 'block';
+        imgBanner.style.position = 'absolute';
+        imgBanner.style.height = screenWidth * 94 / 640 + 'px';
+        imgBanner.style.width = screenWidth + 'px';
+        bannerItem.appendChild(imgBanner);
+        document.body.appendChild(bannerItem);
+        
+        var imgBack = document.createElement('img');
+        imgBack.id = 'imgBack_fixed';
+        imgBack.src = 'http://10.128.136.21:8001/backbtn.jpg';
+        imgBack.style.display = 'block';
+        imgBack.style.position = 'absolute';
+        imgBack.style.top = (screenWidth * 94 / 640) / 4 + 'px';
+        imgBack.style.height = (screenWidth * 94 / 640) / 3 + 'px';
+        imgBack.style.width = (screenWidth * 94 / 640) *2.7/ 3 + 'px';
+        imgBack.style.right = 10 + 'px';
+        bannerItem.appendChild(imgBack);
+        e.fhBannerStatus =  1;
+        e.bdom.bind(imgBack, 'click', e.onBackClick);
+    },
 
     renderLoading: function (slotInfo) {
         var screenWidth = slotInfo.paramObj.rw;
@@ -309,130 +360,18 @@ oojs.define({
     },
     md5:function(a){function h(a,b){var c,d,g,e,f;g=a&2147483648;e=b&2147483648;c=a&1073741824;d=b&1073741824;f=(a&1073741823)+(b&1073741823);return c&d?f^2147483648^g^e:c|d?f&1073741824?f^3221225472^g^e:f^1073741824^g^e:f^g^e}function f(a,b,c,d,g,e,f){a=h(a,h(h(b&c|~b&d,g),f));return h(a<<e|a>>>32-e,b)}function k(a,b,c,d,g,e,f){a=h(a,h(h(b&d|c&~d,g),f));return h(a<<e|a>>>32-e,b)}function l(a,b,d,c,e,g,f){a=h(a,h(h(b^d^c,e),f));return h(a<<g|a>>>32-g,b)}function m(a,b,d,c,g,e,f){a=h(a,h(h(d^(b|~c),g),f));return h(a<<e|a>>>32-e,b)}function n(a){var b="",d="",c;for(c=0;3>=c;c++)d=a>>>8*c&255,d="0"+d.toString(16),b+=d.substr(d.length-2,2);return b}var g=[],e,p,q,r,s,b,c,d;a=function(a){a=a.replace(/\r\n/g,"\n");for(var b="",d=0;d<a.length;d++){var c=a.charCodeAt(d);128>c?b+=String.fromCharCode(c):(127<c&&2048>c?b+=String.fromCharCode(c>>6|192):(b+=String.fromCharCode(c>>12|224),b+=String.fromCharCode(c>>6&63|128)),b+=String.fromCharCode(c&63|128))}return b}(a);g=function(a){var b,c=a.length;b=c+8;for(var d=16*((b-b%64)/64+1),e=Array(d-1),g=0,f=0;f<c;)b=(f-f%4)/4,g=f%4*8,e[b]|=a.charCodeAt(f)<<g,f++;b=(f-f%4)/4;e[b]|=128<<f%4*8;e[d-2]=c<<3;e[d-1]=c>>>29;return e}(a);b=1732584193;c=4023233417;a=2562383102;d=271733878;for(e=0;e<g.length;e+=16)p=b,q=c,r=a,s=d,b=f(b,c,a,d,g[e+0],7,3614090360),d=f(d,b,c,a,g[e+1],12,3905402710),a=f(a,d,b,c,g[e+2],17,606105819),c=f(c,a,d,b,g[e+3],22,3250441966),b=f(b,c,a,d,g[e+4],7,4118548399),d=f(d,b,c,a,g[e+5],12,1200080426),a=f(a,d,b,c,g[e+6],17,2821735955),c=f(c,a,d,b,g[e+7],22,4249261313),b=f(b,c,a,d,g[e+8],7,1770035416),d=f(d,b,c,a,g[e+9],12,2336552879),a=f(a,d,b,c,g[e+10],17,4294925233),c=f(c,a,d,b,g[e+11],22,2304563134),b=f(b,c,a,d,g[e+12],7,1804603682),d=f(d,b,c,a,g[e+13],12,4254626195),a=f(a,d,b,c,g[e+14],17,2792965006),c=f(c,a,d,b,g[e+15],22,1236535329),b=k(b,c,a,d,g[e+1],5,4129170786),d=k(d,b,c,a,g[e+6],9,3225465664),a=k(a,d,b,c,g[e+11],14,643717713),c=k(c,a,d,b,g[e+0],20,3921069994),b=k(b,c,a,d,g[e+5],5,3593408605),d=k(d,b,c,a,g[e+10],9,38016083),a=k(a,d,b,c,g[e+15],14,3634488961),c=k(c,a,d,b,g[e+4],20,3889429448),b=k(b,c,a,d,g[e+9],5,568446438),d=k(d,b,c,a,g[e+14],9,3275163606),a=k(a,d,b,c,g[e+3],14,4107603335),c=k(c,a,d,b,g[e+8],20,1163531501),b=k(b,c,a,d,g[e+13],5,2850285829),d=k(d,b,c,a,g[e+2],9,4243563512),a=k(a,d,b,c,g[e+7],14,1735328473),c=k(c,a,d,b,g[e+12],20,2368359562),b=l(b,c,a,d,g[e+5],4,4294588738),d=l(d,b,c,a,g[e+8],11,2272392833),a=l(a,d,b,c,g[e+11],16,1839030562),c=l(c,a,d,b,g[e+14],23,4259657740),b=l(b,c,a,d,g[e+1],4,2763975236),d=l(d,b,c,a,g[e+4],11,1272893353),a=l(a,d,b,c,g[e+7],16,4139469664),c=l(c,a,d,b,g[e+10],23,3200236656),b=l(b,c,a,d,g[e+13],4,681279174),d=l(d,b,c,a,g[e+0],11,3936430074),a=l(a,d,b,c,g[e+3],16,3572445317),c=l(c,a,d,b,g[e+6],23,76029189),b=l(b,c,a,d,g[e+9],4,3654602809),d=l(d,b,c,a,g[e+12],11,3873151461),a=l(a,d,b,c,g[e+15],16,530742520),c=l(c,a,d,b,g[e+2],23,3299628645),b=m(b,c,a,d,g[e+0],6,4096336452),d=m(d,b,c,a,g[e+7],10,1126891415),a=m(a,d,b,c,g[e+14],15,2878612391),c=m(c,a,d,b,g[e+5],21,4237533241),b=m(b,c,a,d,g[e+12],6,1700485571),d=m(d,b,c,a,g[e+3],10,2399980690),a=m(a,d,b,c,g[e+10],15,4293915773),c=m(c,a,d,b,g[e+1],21,2240044497),b=m(b,c,a,d,g[e+8],6,1873313359),d=m(d,b,c,a,g[e+15],10,4264355552),a=m(a,d,b,c,g[e+6],15,2734768916),c=m(c,a,d,b,g[e+13],21,1309151649),b=m(b,c,a,d,g[e+4],6,4149444226),d=m(d,b,c,a,g[e+11],10,3174756917),a=m(a,d,b,c,g[e+2],15,718787259),c=m(c,a,d,b,g[e+9],21,3951481745),b=h(b,p),c=h(c,q),a=h(a,r),d=h(d,s);g=function(a){var b=a,c=0;for(a=8-a.length;c<a;c++)b="0"+b;return b};b=((parseInt("0x"+n(b),16)+parseInt("0x"+n(c),16))%4294967296).toString(16);a=((parseInt("0x"+n(a),16)+parseInt("0x"+n(d),16))%4294967296).toString(16);8>b.length&&(b=g(b));8>a.length&&(a=g(a));return b+a},
     
+    onBackClick:function(e){
+        var topItem = document.getElementById('fh_top_item');
+        var bannerItem = document.getElementById('fh_banner_item_fixed');
+        topItem.style.position = 'relative';
+        window.scrollTo(0,0);
+        window.fhShowStatus = 0;
+        bannerItem.style.display = 'none';
+    },
     /**
      * 其他功能函数
      */
 
-    createBackupWrapper: function(tu) {
-        try {
-            var scripts = document.getElementsByTagName("script");
-            var targetscript = scripts[scripts.length - 1];
-            var e = this;
-            if (targetscript) {
-                var n = targetscript.parentNode;
-                if (n) {
-                    var new_div = document.createElement("div");
-                    new_div.id = "fh_top_item_" + tu;
-                    new_div.style.width = "100%";
-                    new_div.style.height = "400px";
-                    new_div.style.overflow = "hidden";
-                    new_div.style.boxShadow = '0px 10px 8px #ddd';
-                    new_div.style.backgroundColor = '#fff';
-                    n.insertBefore(new_div, targetscript);
-
-                    this.bdom.bind(new_div, "scroll", function () {
-                        var div_height = new_div.clientHeight;
-                        var doc_height = new_div.scrollHeight;
-                        var scroll_top = new_div.scrollTop;
-                        if (scroll_top + div_height > (doc_height - e.bdom.getClientHeight())){
-                            e.sendAdsRequest();
-                            if (e.tabIdx % 4 === 0)  e.sendAdsRequest();
-                        }
-                    });
-                    var adsDiv = document.createElement('div');
-                    adsDiv.id = 'fh_ads_item_' + tu;
-                    adsDiv.style.width = "100%";
-                    adsDiv.style.height = "auto";
-                    adsDiv.style.borderWidth = '1px 0px 1px 0px';
-                    adsDiv.style.borderStyle = 'solid';
-                    adsDiv.style.borderColor = '#ddd';
-                    this.addTopBanner(new_div);
-                    new_div.appendChild(adsDiv);
-
-                    this.addLoadMoreBtn(n, targetscript, new_div);
-                    return new_div;
-                }
-            }
-        } catch (r) {}
-        return !1;
-    },
-    addTopBanner: function(parent){
-        var topBanner = document.createElement("div");
-        topBanner.style.width = this.adSlotInfo.paramObj.rw + 'px';
-        topBanner.style.marginTop = '5px';
-        topBanner.style.height = 25 + 'px';
-        parent.appendChild(topBanner);
-
-        var topImg = document.createElement('img');
-        topImg.src = "http://10.128.136.21:8001/topBanner.gif";
-        topImg.style.marginLeft = '10px';
-        topBanner.appendChild(topImg);
-
-    },
-
-    addLoadMoreBtn :function(parent, nextNode, new_div){
-        var loadMore = document.createElement("div");
-        loadMore.width = "100%";
-        loadMore.height = "30px";
-        var more_img = document.createElement("img");
-        more_img.id = "clk_more";
-        more_img.src = "http://10.128.136.21:8001/load_more.png";
-        more_img.style.marginTop = '10px';
-        more_img.style.marginLeft = (this.adSlotInfo.paramObj.rw - 75)/2 + 'px';
-        loadMore.appendChild(more_img);
-        parent.insertBefore(loadMore, nextNode);
-        var e = this;
-        this.bdom.bind(loadMore, "click", function()
-        {
-            new_div.style.height = e.bdom.getClientHeight() + 30 +"px";
-            new_div.style.overflow = 'scroll';
-            new_div.style.webkitOverflowScrolling = 'touch';
-            new_div.style.position = 'absolute';
-            new_div.style.top = '0px';
-            new_div.style.zIndex = 2147483647;
-            //document.body.style.height = e.bdom.getClientHeight() + 'px';
-           //document.body.style.height = "100%";
-            //document.documentElement.style.overflow = 'hidden';
-            //window.scrollTo(0,new_div.offsetTop);
-            window.scrollTo(0,0);
-            var backImg = document.getElementById('feihong_imgBack');
-            backImg.style.display = 'block';
-            /*
-            e.bdom.bind(document.body,'touchmove',function(event){
-                event.preventDefault();
-            });
-            */
-        });
-
-    },
-    addBackBtn: function(top,left,slotInfo){
-        var imgBack = document.createElement('img');
-        var screenWidth = slotInfo.paramObj.rw;
-        var e = this;
-        imgBack.id = 'feihong_imgBack';
-        imgBack.src = 'http://10.128.136.21:8001/backbtn.jpg';
-        imgBack.style.display = 'none';
-        imgBack.style.position = 'fixed';
-        imgBack.style.top = (screenWidth * 94 / 640) / 4 + 'px';
-        imgBack.style.height = (screenWidth * 94 / 640) / 3 + 'px';
-        imgBack.style.width = (screenWidth * 94 / 640) *2.7/ 3 + 'px';
-        imgBack.style.right = 10 + 'px';
-        imgBack.style.zIndex = 2147483647;
-        e.fhBannerStatus =  1;
-        e.bdom.bind(imgBack, 'click', function(){
-            var topItem = document.getElementById('fh_top_item_' + slotInfo.slotId);
-            topItem.style.height = "400px";
-            topItem.style.overflow = "hidden";
-            topItem.style.position = 'relative';
-            document.body.style.height = e.bdHeight;
-            document.documentElement.style.overflow = 'auto';
-            document.do
-            var backImg = document.getElementById('feihong_imgBack');
-            backImg.style.display = 'none';
-        });
-        document.body.appendChild(imgBack);
-
-    },
     /**
      * 获取广告容器
      * @param {string} wrapperId 总控传入的容器id
@@ -445,7 +384,7 @@ oojs.define({
             result = document.getElementById(wrapperId);
         }
         return result;
-    }
+    },
 
 
 });
